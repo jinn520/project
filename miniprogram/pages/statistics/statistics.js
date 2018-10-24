28a150372b9d0e584c9ed4a4bbb12a821d90eddb
unknown <1204443635@qq.com>
2018-10-24 17:01:30 +0800

    日历处优化

--- miniprogram/pages/statistics/statistics.js
+++ miniprogram/pages/statistics/statistics.js
@@ -1,66 +1,145 @@
-// pages/statistics/statistics.js
+//index.js
+//获取应用实例
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    dayStylePri: [
+      { month: 'current', day: new Date().getDate(), color: 'white', background: '#AAD4F5' },
+      { month: 'current', day: new Date().getDate(), color: 'white', background: '#AAD4F5' },
+    ],
+    dayStyle: [
+      
+    ],
+    userinfo: [],
+    month: new Date().getMonth() + 1,
+    year: new Date().getFullYear(),
+    day: new Date().getDate(),
+    monthpri: new Date().getMonth() + 1,
+    yearpri: new Date().getFullYear(),
+    daypri: new Date().getDate(),
+    clock: new Date().getHours()
+  },
+  onLoad() {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  dayClick: function (event) {
+    // let clickDay = event.detail.day;
+    // let changeDay = `dayStyle[1].day`;
+    // let changeBg = `dayStyle[1].background`;
+    // this.setData({
+    //   [changeDay]: clickDay,
+    //   [changeBg]: "#FF0000"
+    // })
+    let allDay = this.data.dayStyle
+    allDay.push({ month: 'current', day: 26, color: 'white', background: '#00CED1' })
+    this.setData({
+      dayStyle: allDay
+    })
+    console.log(this.data)
+  },
+  onReady: function(){
+    var that = this
+    wx.getStorage({
+      key: 'userinfo',
+      success(res) {
+        console.log("[自定义函数][statistics] 获取storage信息成功！ ")
+        that.setData({
+          userinfo: res.data
+        })
+        that.updateDate()
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  updateDate: function(){
+    let that = this
+    let mealInfo = {
+      userid: this.data.userinfo[0]["id"],
+      year: this.data.year,
+      month: this.data.month
+    }
+    wx.request({
+      url: 'https://jinn520.club/stopmeal/getstopmeal',
+      data: mealInfo,
+      method: 'POST',
+      success: function (res) {
+        for(let i = 0; i < res.data.length; i++){
+          let dayOfMonth = res.data[i]['date']
+          let sign = res.data[i]['sign']
+          that.addColor(dayOfMonth,sign)
+        }
+      } 
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  addColor: function (dayOfMonth, sign){
+    let allDay = this.data.dayStyle
+    let middle = dayOfMonth.split("-")
+    let day = middle[2]
+    if (day < this.data.day){
+      if(sign == 0){
+        allDay.push({ month: 'current', day: day, color: 'white', background: '#90EE90' })
+      }
+    } else if (day > this.data.day){
+      allDay.push({ month: 'current', day: day, color: 'white', background: '#32CD32' })
+    }else {
+      if(this.data.clock >= 9){
+        allDay.push({ month: 'current', day: day, color: 'white', background: '#90EE90' })
+      }else {
+        allDay.push({ month: 'current', day: day, color: 'white', background: '#32CD32' })
+      }
+    }
+    this.setData({
+      dayStyle: allDay
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  next: function(e){
+    let monthmiddle = this.data.month + 1
+    if(monthmiddle > 12){
+      this.setData({
+        year: this.data.year + 1,
+        month: 1,
+        dayStyle: []
+      })
+    }else {
+      this.setData({
+        month: this.data.month + 1,
+        dayStyle: []
+      })
+    }
+    if(this.data.month == this.data.monthpri && this.data.year == this.data.yearpri){
+      this.updateDate()
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  prev: function(e){
+    let monthmiddle = this.data.month - 1
+    if (monthmiddle < 1) {
+      this.setData({
+        year: this.data.year - 1,
+        month: 12,
+        dayStyle: []
+      })
+    } else {
+      this.setData({
+        month: this.data.month - 1,
+        dayStyle: []
+      })
+    }
+    if (this.data.month == this.data.monthpri && this.data.year == this.data.yearpri) {
+      this.updateDate()
+    }
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  dateChange: function(e){
+    console.log(e.detail)
+    this.setData({
+      year: e.detail.currentYear,
+      month: e.detail.currentMonth,
+      dayStyle: []
+    })
+    if (this.data.month == this.data.monthpri && this.data.year == this.data.yearpri) {
+      this.updateDate()
+    }
   }
 })
